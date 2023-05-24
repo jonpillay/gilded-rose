b1c1cdd4a373c429b351613a96cd10d9175d00aa
Jon Pillay <jonpillay@MASTUDENT-Jon-Pillay-C02G5H47Q6L4.local>
2023-05-24 20:29:34 +0100

    Tests written covering the code as it's written now. All passing. Lines 53-54 not covered, however I think it's redundant code as far as I understand. I can now refactor and so long as these pass then I've not broken anything

---
+++ js-jest/test/gilded_rose.test.js
@@ -0,0 +1,56 @@
+const {Shop, Item} = require("../src/gilded_rose");
+
+describe("Gilded Rose, tests that cover original functionality of the program", function() {
+  it("should initialise a shop and item and return the name 'foo'", function() {
+    const gildedRose = new Shop([new Item("foo", 0, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].name).toBe("foo");
+  });
+  it("returns the quality -1 after updateQuality (9)", function() {
+    const gildedRose = new Shop([new Item("foo", 20, 10)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(9);
+  });
+  it("returns the quality -2 after updateQuality when sell by date has passed (8)", function() {
+    const gildedRose = new Shop([new Item("foo", -1, 10)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(8);
+  });
+  it("should not decrease an item's quality below 0", function() {
+    const gildedRose = new Shop([new Item("foo", 20, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(0);
+  });
+  it("should initialise an 'Aged Brie' item and returns the quality +1 after updateQuality (11)", function() {
+    const gildedRose = new Shop([new Item("Aged Brie", 20, 10)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(11);
+  });
+  it("should initialise an 'Aged Brie' item an not increase its quality above 50", function() {
+    const gildedRose = new Shop([new Item("Aged Brie", 20, 50)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(50);
+  });
+  it("should initialise an 'Backstage passes to a TAFKAL80ETC concert' item and increase quality by 1", function() {
+    const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 20, 10)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(11);
+  });
+  it("should initialise an 'Backstage passes to a TAFKAL80ETC concert' item and increase quality by 2 (9 days away)", function() {
+    const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 9, 10)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(12);
+  });
+  it("should initialise an 'Backstage passes to a TAFKAL80ETC concert' item and increase quality by 3 (2 days away)", function() {
+    const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 2, 10)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(13);
+  });
+  it("should initialise an 'Backstage passes to a TAFKAL80ETC concert' item and decrease quality to 0 (sellBy/concert passed)", function() {
+    const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 0, 10)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(0);
+  });
+});
+
+'Backstage passes to a TAFKAL80ETC concert'
